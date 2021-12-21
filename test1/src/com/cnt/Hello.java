61cf1d1e361cd77170fccc88f6b8a01472111e72
nantao <nantao.chen@fit2cloud.com>
2021-12-21 17:30:01 +0800

    idea测试提交

---
+++ test1/src/com/cnt/Hello.java
@@ -0,0 +1,13 @@
+package com.cnt;
+
+/**
+ * @program: test1
+ * @description: tes
+ * @author: nantao.chen
+ * @create: 2021-12-20 15:32
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world!!!");
+    }
+}
