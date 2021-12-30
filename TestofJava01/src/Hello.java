4439113493a74fcb277f081fb001a08712e353ae
nantao <nantao.chen@fit2cloud.com>
2021-12-30 11:55:20 +0800

    hello 文件

---
+++ TestofJava01/src/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @program: TestofJava01
+ * @description: hello world
+ * @author: nantao.chen
+ * @create: 2021-12-30 11:53
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
